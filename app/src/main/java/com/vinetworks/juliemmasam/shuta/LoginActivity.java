6c2c412a20d488b0d9db36503a71cc751c04bd89
Emmanuel Julius <juliemmasam@gmail.com>
2019-01-13 01:07:03 +0300

    Initial Commit

---
+++ app/src/main/java/com/vinetworks/juliemmasam/shuta/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.vinetworks.juliemmasam.shuta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
